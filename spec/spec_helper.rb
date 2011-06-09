7d5e12b21cbd8746a203c8663e4a875928ce31bf
Wlodek Bzyl <matwb@univ.gda.pl>
2011-06-10 00:10:51 +0200

    Removed url-for files.

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'spec'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-Spec::Runner.configure do |config|
-  
-end
